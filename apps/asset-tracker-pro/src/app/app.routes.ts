6acd23358495ca562ecdf4187e71515dde58ccc9
Marek Skotny <marco.personal.development@gmail.com>
2026-04-03 12:46:49 +0200

    [ATP-8] - cleaning

--- apps/asset-tracker-pro/src/app/app.routes.ts
+++ apps/asset-tracker-pro/src/app/app.routes.ts
@@ -6,30 +6,4 @@ export const appRoutes: Route[] = [
   { path: '', redirectTo: 'add-asset', pathMatch: 'full' },
   { path: 'add-asset', component: AddAssetComponent },
   { path: 'show-summary', component: ShowSummaryComponent },
-  {
-    path: 'table',
-    loadComponent: () =>
-      import('./playground/table.component').then((m) => m.TableComponent),
-  },
-  {
-    path: 'counter',
-    loadComponent: () =>
-      import('./playground/counter/counter.component').then(
-        (m) => m.CounterComponent
-      ),
-  },
-  {
-    path: 'addproduser',
-    loadComponent: () =>
-      import('./playground/prod-users/add/add-user.component').then(
-        (m) => m.AddUserComponent
-      ),
-  },
-  {
-    path: 'showprodusers',
-    loadComponent: () =>
-      import('./playground/prod-users/show/show-users.component').then(
-        (m) => m.ShowUsersComponent
-      ),
-  },
 ];
--- libs/users/README.md
+++
@@ -1,3 +0,0 @@
-# users-store
-
-This library was generated with [Nx](https://nx.dev).
